07be3c4749954b34e850e41c272edae0ba18506b
Raghesh99 <raghesh99@gmail.com>
2021-06-16 17:27:32 +0200

    up

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,9 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import {
+  Directive,
+  HostBinding,
+  HostListener,
+  ElementRef,
+} from "@angular/core";
 
 @Directive({
   selector: "[appDropdown]",
@@ -6,8 +11,16 @@ import { Directive, HostBinding, HostListener } from "@angular/core";
 export class DropdownDirective {
   @HostBinding("class.open") isOpen: boolean = false;
 
-  @HostListener("click") toggleOpen() {
+  /*   @HostListener("click") toggleOpen() {
     this.isOpen = !this.isOpen;
   }
-  constructor() {}
+  constructor() {} */
+
+  //* Così si chiude se clicchiamo da qualsiasi altra parte nella pagina
+  @HostListener("document:click", ["$event"]) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
+  }
+  constructor(private elRef: ElementRef) {}
 }
